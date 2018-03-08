c819ff539bd1eee3232692d04c08f1dbd301acab
patnolan33 <pat.nolan@heronsystems.com>
2018-03-07 20:59:49 -0500

    Added TCP socket to DetectMotion script

--- detectMotion/DetectMotion.ts
+++ detectMotion/DetectMotion.ts
@@ -1,5 +1,40 @@
+var net = require('net');
 var five = require("johnny-five");
 var board = new five.Board();
+
+function makeTCPRequest(messagType: string, messageData: string) {
+  let socket = new net.Socket();
+  setupTCPSocket(socket);
+  socket.connect(1234, '127.0.0.1', function() {
+    let tcpRequest = {
+      messageType: messagType,
+      messageData: messageData
+    };
+    socket.write(JSON.stringify(tcpRequest));
+    socket.end();
+  });
+}
+
+function setupTCPSocket(socket: any) {
+  // Add a 'data' event handler for the client socket
+  // data is what the server sent to this socket
+  socket.on('data', function(data: any) {
+      console.log('DATA: ' + data);
+  });
+
+  // Add a 'close' event handler for the client socket
+  socket.on('close', function() {
+      console.log('Connection closed');
+      socket.destroy();
+  });
+
+  // Add an 'error' event handler
+  socket.on('error', function(err: any) {
+      console.log('Socket ' + err);
+      socket.destroy();
+  });
+}
+
 
 board.on("ready", function() {
 
@@ -15,12 +50,14 @@ board.on("ready", function() {
   // proximal area is disrupted, generally by some form of movement
   motion.on("motionstart", function() {
     console.log("motionstart");
+    makeTCPRequest('motionStart', 'true');
   });
 
   // "motionend" events are fired following a "motionstart" event
   // when no movement has occurred in X ms
   motion.on("motionend", function() {
     console.log("motionend");
+    makeTCPRequest('motionStart', 'false');
   });
 
   // "data" events are fired at the interval set in opts.freq
