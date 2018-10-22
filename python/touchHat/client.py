66286c25a963b2a9fc637c0f0acd6ad5f0dea836
Patrick Nolan <patnolan33@gmail.com>
2018-10-21 20:24:02 -0400

    Adding touchHat files for Pi to Pi comms with touch tape

---
+++ python/touchHat/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import socket
+import json
+
+
+TCP_IP = '10.0.0.93'
+TCP_PORT = 10000
+BUFFER_SIZE = 1024
+MESSAGE = json.dumps({
+    "turnOn": True,
+    "lightColor": "#000000"
+})
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
